ff73dec23c4fa4c564b62feab4c2db8d9e6224b9
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2015-09-09 14:42:43 +0200

    moved text box for collection property. Hide property if empty

---
+++ src/Solvberget.Search8/StringToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Solvberget.Search8
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (String.IsNullOrEmpty((string)value) || (string)value == "Ukjent")
+            {
+                return Visibility.Collapsed;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
