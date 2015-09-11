f41df45a6bcc46beacec27aab921fe95b0d85675
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2015-09-11 17:05:29 +0200

    maintained aspect ratio for pictures for cd covers and audio books

---
+++ src/Solvberget.Search8/DocumentImageHeightConverter.cs
@@ -0,0 +1,41 @@
+﻿using Solvberget.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Solvberget.Search8
+{
+    class DocumentImageHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            // for results view
+            if (value is string)
+            {
+                if((string)value == "Cd" || (string)value == "AudioBook")
+                {
+                    return Stretch.Uniform;
+                } else
+                {
+                    return Stretch.Fill;
+                }
+            }
+            
+            // for detail result view
+            if (value is CdDto || value is AudioBookDto)
+            {
+                return 300;
+            }
+            return 463;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
